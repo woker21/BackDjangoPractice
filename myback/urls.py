416d0b732bc5c8019a83e1b8605608ae3e641d6f
Woker21 <112359676+woker21@users.noreply.github.com>
2024-08-19 17:57:55 +0200

    update

--- myback/urls.py
+++ myback/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from perfil.views import helloPeople
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', helloPeople),
 ]
--- subida/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubidaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'subida'
